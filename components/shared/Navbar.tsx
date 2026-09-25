fd11e8913e735d03424fdc1596fb9d3b5eea505d
Harshit Aryan <harshitaryan21@gmail.com>
2026-09-25 17:00:29 +0530

    Rework navbar and drop command center tabs

--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import Link from "next/link";
-import Image from "next/image";
 import { useEffect, useRef, useState } from "react";
 import { useT } from "@/lib/languageContext";
 import LanguageSelector from "@/components/shared/LanguageSelector";
